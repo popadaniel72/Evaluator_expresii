1e5cd92d63ec675b5ab046314da8ce7124100ec1
popadaniel72 <popadaniel72@yahoo.com>
2024-02-16 13:00:31 +0200

    Add project files.

---
+++ Evaluator_expresii/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Evaluator_expresii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.expr = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lb = new System.Windows.Forms.ListBox();
+            this.pentruDesen = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // expr
+            // 
+            this.expr.Location = new System.Drawing.Point(18, 18);
+            this.expr.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.expr.Name = "expr";
+            this.expr.Size = new System.Drawing.Size(478, 26);
+            this.expr.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(540, 9);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Arbore";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lb
+            // 
+            this.lb.FormattingEnabled = true;
+            this.lb.ItemHeight = 20;
+            this.lb.Location = new System.Drawing.Point(1038, 68);
+            this.lb.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(178, 624);
+            this.lb.TabIndex = 2;
+            // 
+            // pentruDesen
+            // 
+            this.pentruDesen.Location = new System.Drawing.Point(18, 52);
+            this.pentruDesen.Name = "pentruDesen";
+            this.pentruDesen.Size = new System.Drawing.Size(1013, 459);
+            this.pentruDesen.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1263, 774);
+            this.Controls.Add(this.pentruDesen);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.expr);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox expr;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lb;
+        private System.Windows.Forms.Panel pentruDesen;
+    }
+}
+
---
+++ Evaluator_expresii/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluator_expresii
+{
+    
+    
+
+    public unsafe partial class Form1 : Form
+    {
+        unsafe class Nod
+        {
+            public char op;
+            public int x;
+            public Nod st;
+            public Nod dr;
+            public Nod(char o, int vx, Nod s, Nod d)
+            {
+                op = o; x = vx; st = s; dr = d;
+            }
+            public Nod()
+            {
+                op = ' '; x = 0; st = null; dr = null;
+            }
+        }       
+        int nrm = 100, rx = 20, ry = 10, dx = 160, dy = 30;
+        string[] atomi;
+        string semne = "+-/%*()";
+        int nrAtomi, poz;
+        Nod rad;
+        Graphics ptDesen;
+        Pen cLinii;
+        Font font = new Font("Arial", 12);
+
+        SolidBrush culOp = new SolidBrush(Color.Red);
+        SolidBrush culVal = new SolidBrush(Color.Blue);
+        public Form1()
+        {
+            InitializeComponent();
+            atomi = new string[nrm];
+            nrAtomi = 0;
+            dx = pentruDesen.Width / 4;
+            ptDesen = pentruDesen.CreateGraphics();
+            cLinii = new Pen(Color.Black,1);
+
+        }
+        private void atomizeaza(string s)
+        {
+            int i = 0;
+            string at;
+            nrAtomi = 0; poz = 1;
+            while(i<s.Length)
+            {
+                if (semne.IndexOf(s[i]) != -1) atomi[++nrAtomi] = s[i++].ToString();
+                else
+                {
+                    at = "";
+                    while (i < s.Length && semne.IndexOf(s[i]) == -1)
+                        at += s[i++].ToString();
+                    atomi[++nrAtomi] = at;
+                }
+
+            }
+        }
+        Nod termen()
+        {
+            Nod b=null, c=null; char o;
+            b = factor();
+            while (atomi[poz] == "*" || atomi[poz] == "/" || atomi[poz] == "%")
+            {
+                o = atomi[poz][0];
+                poz++;
+                c = factor();
+                b = new Nod(o, 0, b, c);
+            }
+            return b;
+        }
+        Nod factor()
+        {
+            Nod f=null; int x=0; 
+            if(atomi[poz]=="(")
+            {
+                poz++;
+                f = expresie();
+                if (atomi[poz] != ")") MessageBox.Show("Lipseste )");
+                poz++;
+            }
+            else
+            {
+                x = Convert.ToInt32(atomi[poz]);
+                poz++;
+                f = new Nod(' ', x, null, null);
+            }
+            return f;
+        }
+        Nod expresie()
+        {
+            Nod b, c; char o;
+            b = termen();
+            while(atomi[poz]=="+" || atomi[poz]=="-")
+            {
+                o = atomi[poz][0];
+                poz++;
+                c = termen();
+                b = new Nod(o, 0, b, c);
+            }
+            return b;
+        }
+        void kaput(Nod x)
+        {
+            if(x!=null)
+            {
+                kaput(x.st);
+                kaput(x.dr);
+                x = null;
+            }
+        }
+        void afis(Nod c, int n, int x, int y)
+        {
+            if (c == null) return;
+            ptDesen.DrawEllipse(cLinii, x-rx, y, 2 * rx, 2 * ry);
+            if (c.op != ' ') ptDesen.DrawString(c.op.ToString(), font,culOp , x- c.op.ToString().Length*4, y);
+              else
+                ptDesen.DrawString(c.x.ToString(), font, culOp, x - c.x.ToString().Length * 4, y);
+            /// de scris in elipsa
+            if (c.st!=null)
+            {
+                ptDesen.DrawLine(cLinii, x, y, x - dx / n, y + dy);
+                afis(c.st, n + 1, x - dx / n, y + dy);
+            }
+            if(c.dr!=null)
+            {
+                ptDesen.DrawLine(cLinii, x, y, x + dx / n, y + dy);
+                afis(c.dr, n + 1, x + dx / n, y + dy);
+            }
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            atomizeaza(expr.Text);
+            rad = null;
+            rad = expresie();
+            afis(rad, 1, pentruDesen.Width / 2, 2 * ry);
+            lb.Items.Clear();
+            for (int i = 1; i <= nrAtomi; i++)
+                lb.Items.Add(atomi[i]);
+        }
+    }
+}
